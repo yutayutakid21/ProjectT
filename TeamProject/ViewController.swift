c2f7dbd13e9a6ba950a4d46e9648d9a96f728eed
Yuta Fujii <yutayutakid21@gmail.com>
2019-11-13 18:19:43 +0900

    課題

--- TeamProject/ViewController.swift
+++ TeamProject/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //近藤さん・・・TableViewを作成し、配列に5つの要素を追加し、cell.labelに表示してください。セルをタップしたときに画面遷移をID nextでlabel.textを値を渡しながら遷移させてください。画面遷移先はNextViewControllerです。
+        //また、遷移先で受け取る変数名はtextStringです。
+    
+    
+    //藤井・・・受け取った値をラベルに表示します。
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
